4cccb4522d4f2c43388a5f524187c3826662daf7
sinework <sine.s.amare@gmail.com>
2020-10-11 18:44:41 +0300

    Add: created bot class andintitaized token and other variables

--- lib/bot.rb
+++ lib/bot.rb
@@ -3,3 +3,25 @@ require 'net/http'
 require 'json'
 
 require_relative './caller.rb'
+class Bot
+    attr_accessor :general, :token, :question_object, :dupe, :ans, :user_score, :user_answer
+   def initialize
+    @token = '1294334531:AAGenQ2e-OQlO-VdYs-Sywk2KI7saIxXAyM'
+    @question_object=Hash.new()
+    @dupe=[]
+    @ans=[]
+    @user_answer=''
+    @ind=''
+    @user_score={
+      num_of_questions: 0,
+      correct_ans:0
+    }
+    
+   end
+
+   
+
+
+
+
+end   
