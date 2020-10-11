bad801f67d14e36c574f3b09390f2dfcd344cccd
sinework <sine.s.amare@gmail.com>
2020-10-11 18:55:04 +0300

    Add: intitialized the class and created the requesting method.

--- lib/general_knowledge.rb
+++ lib/general_knowledge.rb
@@ -6,4 +6,14 @@ require 'json'
 class General_knowledge
   @values = nil
   
-   
+  def initialize
+    @values = make_the_request
+  end
+
+  def make_the_request
+    url = 'https://opentdb.com/api.php?amount=10&category=9&type=multiple'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response = JSON.parse(response)
+    response["results"].sample
+  end
