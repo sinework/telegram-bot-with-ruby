ce82eb76b52b45d55a63ede9c5919d6b26ceef9b
sinework <sine.s.amare@gmail.com>
2020-10-12 12:26:16 +0300

    Update: Updated Rspec

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,15 +1,20 @@
 require_relative '../lib/bot'
+
 describe Bot do
   let(:bot) { Bot.new }
+
   it 'Raises argument error when arguments are given' do
     expect { bot.main('value') }.to raise_error(ArgumentError)
   end
+
   it 'Raises argument error when arguments are given' do
     expect { bot.check_win('value') }.to raise_error(ArgumentError)
   end
+
   it 'Raises argument error when arguments are given' do
     expect { bot.general_method('value') }.to raise_error(ArgumentError)
   end
+
   it 'Raises argument error when arguments are given ' do
     expect { bot.wrong_answer('value') }.to raise_error(ArgumentError)
   end
